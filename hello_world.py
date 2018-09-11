d768165979da4011fb5f757789eebc7793b709a9
dsmdavid <dsmdavid@gmail.com>
2018-09-11 19:53:16 +0100

    reverting something

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 print ('hello world')
 
 print(range(1:10))
 =======
 print ('CIAO MINA')
 print('ciao Ollie')
+=======
+print ('hello world')
+>>>>>>> parent of 8d269b5... Range Print
